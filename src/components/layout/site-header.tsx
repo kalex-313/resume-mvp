61a1e643f46b65045b174910b7bdf71292ded8b5
Kalex Wong <matekeki@gmai.com>
2026-03-31 13:14:59 -0700

    Update header logo UI

--- src/components/layout/site-header.tsx
+++ src/components/layout/site-header.tsx
@@ -13,7 +13,7 @@ export async function SiteHeader() {
           <img
             src="/logo.png"
             alt="CVForge"
-            className="h-16 w-auto object-contain"
+            className="h-20 w-auto object-contain"
           />
         </Link>
 
